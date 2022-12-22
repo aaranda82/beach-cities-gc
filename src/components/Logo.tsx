60cee9def682d646fce789452aca21d6dde33641
Alex Aranda <alex.aranda@vincit.fi>
2022-12-21 23:51:48 -0800

    Update logo size conditionally

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../public/logo.jpg";
 
 const Logo: React.FC = () => {
-  const width = 100;
+  const [width, setWidth] = useState(100);
+  const hasWindow = typeof window !== "undefined";
+  useEffect(() => {
+    if (hasWindow && window.innerWidth > 500) setWidth(300);
+  }, [hasWindow]);
   return (
     <Image
       src={logo}
       alt="Beach cities general construction logo"
       width={width}
-      height={width * 0.71}
     />
   );
 };
